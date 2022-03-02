39b5568e262ea67bca70d07cf76f37a2c4fec8a8
momin109 <momin550550@gmail.com>
2022-03-02 18:45:59 +0600

    five commit

--- js/app.js
+++ js/app.js
@@ -9,3 +9,25 @@ const searchPhone = () => {
             .then(res => res.json())
             .then(data => displaySearchResult(data.data.slice(0, 20)))
 }
+
+const displaySearchResult = phones => {
+    const searshResult = document.getElementById('seach-result')
+    searshResult.textContent = '';
+    phones.forEach(phone => {
+        // console.log(phone);
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card w-75">
+            <img src="${phone.image}" class="card-img-top p-4" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+                <button onclick="LoadPhoneDetail('${phone.slug}')" class="bg-primary text-white">see details </button>
+            
+             </div>
+         </div>
+        `;
+        searshResult.appendChild(div);
+    });
+}
